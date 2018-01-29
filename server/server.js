4b253bac10611c12568dbda4d009eb6d79077788
Covalence Projects <paytonhaisten@gmail.com>
2018-01-29 15:31:33 -0600

    Phase one complete

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const tater = require("path");
+const fs = require("fs");
+
+let chirps = [
+  {
+    name: "Payton",
+    message: "This is a chirp, bud."
+  },
+  {
+    name: "Payton",
+    message: "Woke up and chirped."
+  },
+  {
+    name: "Payton",
+    message: "Chirp town."
+  },
+  {
+    name: "Payton",
+    message: "Went to the chirp zone."
+  },
+  {
+    name: "Payton",
+    message: "Seems a bit chirpy outside today."
+  }
+];
+
+const dataPath = tater.join(__dirname, "../chirps.json");
+
+let sendChirps = JSON.stringify(chirps);
+
+fs.writeFile(dataPath, sendChirps, err => {
+  if (err) console.log("there was an err", err);
+});
+
+fs.readFile(dataPath, function read(err, data) {
+  if (err) {
+    console.log("This is an error");
+  }
+  let derp = JSON.parse(data);
+  derp.forEach(item => {
+    console.log(`${item.name} chirped ${item.message}`);
+  });
+});
